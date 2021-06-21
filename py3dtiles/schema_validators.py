a5a134e5bfbf4bacdd45200b2069e4f2ac3a5fce
EricBoix <eric.boix@insa-lyon.fr>
2021-06-21 23:48:00 +0200

    Fix of package_data installation.

--- py3dtiles/schema_validators.py
+++ py3dtiles/schema_validators.py
@@ -2,18 +2,24 @@
 import os
 import sys
 import json
-import jsonschema
 import pathlib
+import pkg_resources
+import jsonschema
 
 from .threedtiles_core_schemas import ThreeDTilesCoreSchemas
 from .batch_table_hierarchy_extension_schemas import BatchTableHierarchySchemas
 from .temporal_extension_schemas import TemporalExtensionSchemas
 
+# FIXME: if we consider the class attributes that this class uses, this
+# class is probably a singleton (refer to e.g.
+# https://python-patterns.guide/gang-of-four/singleton/) and should be
+# implemented as such.
 class SchemaValidators:
     """
     Dictionary holding the set of validated schemas. The dictionary key is
     the name of the schema as encountered in the "title" property of the schema.
     """
+
     schemas = None
     """
     Dictionary with the class_names (i.e. the name of the classes inheriting
@@ -29,39 +35,42 @@ class SchemaValidators:
     resolver = None
 
     def __init__(self):
+        """
+        Schema_directory is the directory where all the schema files are
+        to be encoutered (concerning this strong constaint refer to
+        to the warning of SchemaWithSample.set_schema_file_path()). But
+        this class has two contexts of usage depending on the context
+        of usage of the py3dtiles package that can be
+        - at package developing stage where the schema directory is a
+          relative path (within the directory layout of the sources)
+        - at package post-installation stage where the schema directory is
+          an absolute path because it is relative to python's directory
+          path of its installed packages (that would the absolute sys.prefix
+          directory path, if they where no python-eggs and
+          other advanced package features).
+        """
+        self.schema_directory = "py3dtiles/jsonschemas"
+        if not os.path.isdir(self.schema_directory):
+            # We had no success in relative context. Could this be an
+            # absolute path context ?
+            if not pkg_resources.resource_isdir("py3dtiles", "jsonschemas/"):
+                print("Unfound jsonschemas directory in installed package.")
+                sys.exit(1)
+            self.schema_directory = pkg_resources.resource_filename(
+                "py3dtiles", "jsonschemas/"
+            )
+
         if not self.schemas:
             self.schemas = dict()
             self.class_names = dict()
-
-            # They are two contexts of usage for this constructor that must
-            # set up the directory where all the schema files are located:
-            #  - a package developing stage where the directory is a relative 
-            #    path (within the directory layout of the sources)
-            #  - a package post-installation stage where the directory is 
-            #    absolute because it is relative to the python's directory 
-            #    path of its installed packages (that would the absolute 
-            #    sys.prefix directory path, if they where no python-eggs and
-            #    other advanced package features).
-            # 
-            # The relative directory context directory (when working within the
-            # source tree):
-            schema_directory = 'py3dtiles/jsonschemas'
-            if not os.path.isdir(schema_directory):
-                # We had no success in relative context. Could this be an 
-                # absolute path context ?
-                installed_path=pathlib.Path(__file__).parent.absolute()
-                schema_directory = os.path.join(installed_path, "jsonschemas")
-                if not os.path.isdir(schema_directory):
-                    print('Failed to establish an installed package context:')
-                    print(f'unfound directory path {schema_directory}')
-                    print('Exiting.')
-                    sys.exit(1)
 
             # sub-schemas within the same directory (provided as absolute path)
             # as the given schema. Refer to
             #     https://github.com/Julian/jsonschema/issues/98
             # for the reasons of the following parameters and call
-            base_uri = pathlib.Path(os.path.abspath(schema_directory)).as_uri() + '/'
+            base_uri = (
+                pathlib.Path(os.path.abspath(self.schema_directory)).as_uri() + "/"
+            )
             self.resolver = jsonschema.RefResolver(base_uri, None)
 
             self.register_schema_with_sample_list(ThreeDTilesCoreSchemas())
@@ -75,56 +84,56 @@ class SchemaValidators:
     def register_schema_with_sample(self, schema_with_sample):
         file_name = schema_with_sample.get_schema_file_path()
         if not os.path.isfile(file_name):
-            print(f'No such file as {file_name}')
+            print(f"No such file as {file_name}")
             sys.exit(1)
 
         try:
-            with open(file_name, 'r') as schema_file:
+            with open(file_name, "r") as schema_file:
                 schema = json.loads(schema_file.read())
         except:
-            print(f'Unable to parse schema held in {file_name}')
+            print(f"Unable to parse schema held in {file_name}")
             sys.exit(1)
 
         try:
-            title = schema['title']
+            title = schema["title"]
         except:
-            print('Schema argument misses a title. Dropping extension.')
+            print("Schema argument misses a title. Dropping extension.")
             sys.exit(1)
 
         key = schema_with_sample.get_key()
         if title in self.schemas:
-            if not key in 'BoundingVolume':
+            if not key in "BoundingVolume":
                 # This is a legitimate case where some classes share the
                 # same validator
                 pass
             else:
-                print(f'Class {key} already has schema named {title}.')
+                print(f"Class {key} already has schema named {title}.")
                 sys.exit(1)
         else:
-            validator = jsonschema.Draft7Validator(schema, resolver = self.resolver)
+            validator = jsonschema.Draft7Validator(schema, resolver=self.resolver)
 
             try:
                 # Strangely enough, in order to validate the schema itself, we
                 # do need to provide a sample complying with the json format:
                 validator.validate(schema_with_sample.get_sample())
             except jsonschema.exceptions.SchemaError:
-                print(f'Invalid schema {title}')
+                print(f"Invalid schema {title}")
                 sys.exit(1)
-            self.schemas[title] = {'schema': schema, 'validator': validator}
+            self.schemas[title] = {"schema": schema, "validator": validator}
         self.class_names[key] = title
 
     def get_validator(self, class_name_key):
         if not class_name_key in self.class_names:
-            print(f'Unregistered schema (class) key {class_name_key}')
+            print(f"Unregistered schema (class) key {class_name_key}")
             return None
         title = self.class_names[class_name_key]
         if not title in self.schemas:
-            print(f'Unregistered schema with title {title}')
+            print(f"Unregistered schema with title {title}")
             return None
         try:
             return self.schemas[title]["validator"]
         except:
-            print(f'Cannot find validator for schema {class_name_key}')
+            print(f"Cannot find validator for schema {class_name_key}")
         return None
 
     def __contains__(self, schema_name):
--- setup.py
+++ setup.py
@@ -6,37 +6,35 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 
 requirements = (
-    'numpy',
-    'pyproj',
-    'cython',
-    'triangle',
-    'psycopg2-binary',
-    'liblas',
-    'laspy',
-    'numba',
-    'pyproj',
-    'psutil',
-    'lz4',
-    'pyzmq',
-    'jsonschema',
-    'pywavefront',
-    'rdp',
-    'shapely'
+    "numpy",
+    "pyproj",
+    "cython",
+    "triangle",
+    "psycopg2-binary",
+    "liblas",
+    "laspy",
+    "numba",
+    "pyproj",
+    "psutil",
+    "lz4",
+    "pyzmq",
+    "jsonschema",
+    "pywavefront",
+    "rdp",
+    "shapely",
 )
 
 dev_requirements = (
-    'pytest',
-    'pytest-cov',
-    'pytest-benchmark',
-    'line_profiler'
+    "black",
+    "pytest",
+    "pytest-cov",
+    "pytest-benchmark",
+    "line_profiler",
 )
 
 doc_requirements = (
-    'sphinx',
-    'sphinx_rtd_theme',
-)
-
-prod_requirements = (
+    "sphinx",
+    "sphinx_rtd_theme",
 )
 
 
@@ -49,46 +47,45 @@ def find_version(*file_paths):
     see https://github.com/pypa/sampleproject/blob/master/setup.py
     """
 
-    with open(os.path.join(here, *file_paths), 'r') as f:
+    with open(os.path.join(here, *file_paths), "r") as f:
         version_file = f.read()
 
     # The version line must have the form
     # __version__ = 'ver'
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError("Unable to find version string. "
-                       "Should be at the first line of __init__.py.")
+    raise RuntimeError(
+        "Unable to find version string. " "Should be at the first line of __init__.py."
+    )
 
 
 setup(
-    name='py3dtiles',
-    version=find_version('py3dtiles', '__init__.py'),
+    name="py3dtiles",
+    version=find_version("py3dtiles", "__init__.py"),
     description="Python module for 3D tiles format",
-    long_description=read('README.rst'),
-    url='https://github.com/Oslandia/py3dtiles',
-    author='Oslandia',
-    author_email='contact@oslandia.com',
-    license='Apache License Version 2.0',
+    long_description=read("README.rst"),
+    url="https://github.com/Oslandia/py3dtiles",
+    author="Oslandia",
+    author_email="contact@oslandia.com",
+    license="Apache License Version 2.0",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
     packages=find_packages(),
     install_requires=requirements,
     test_suite="tests",
     extras_require={
-        'dev': dev_requirements,
-        'prod': prod_requirements,
-        'doc': doc_requirements
+        "dev": dev_requirements,
+        "doc": doc_requirements,
     },
     entry_points={
-        'console_scripts': ['py3dtiles=py3dtiles.command_line:main'],
+        "console_scripts": ["py3dtiles=py3dtiles.command_line:main"],
     },
     include_package_data=True,
-    #packaged_data={"py3dtiles": ["py3dtiles/jsonschemas/*.json"]},
-    zip_safe=False  # zip packaging conflicts with Numba cache (#25)
+    package_data={"py3dtiles": ["py3dtiles/jsonschemas/*.json"]},
+    zip_safe=False,  # zip packaging conflicts with Numba cache (#25)
 )
